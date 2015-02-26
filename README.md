e74377132ce7c263c008d11821514d94b4b8a807
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-02-26 09:32:47 +0800

    The LSCP-Processor node_module.

---
+++ README.md
@@ -0,0 +1,35 @@
+# LSCP Processor Utils
+This module contains utilities for processing lead score files provided by Call Source.
+
+What does this module do?
+* Correctly parsing the CSV files provided by Call Source.
+* Properly formatting known fields (at the moment) for storage purposes, and
+dynamically adding formatting schemes to data when needed.
+* Storing the parsed content of the CSV files into a MongoDB datastore.
+* Balancing the performance needs (mostly concerned with the data
+insert latency) and the quality of the data produced by the application.
+* Storing the data from the CSV files in a way where retrieval of appropriate
+information is straight forward - Since the time zones of the dates are not
+specified, the dates will be stored using an ISO Date String format (ISO 8601)
+having a UTC offset of +0000. Rationale: every file contains calls processed
+objects in which their processingEndTime (for ls scores) and elsProcessingTime
+(for els scores) are in the same day for ex. when processing a file name called
+EnhancedLeadScoreCallProcessed20141231.csv the files shall contain call processed
+objects having an elsProcessingEnd within December 31, 2014 UTC, after parsing
+the csv file.
+* Ensuring that every data present in the CSV files are stored in the database.
+* Ensuring that there are no duplicate data in the MongoDB data base to some
+degree (currently using sha-1 hashes to ensure uniqueness of the duplicated callIds).
+
+## The CSV to JavaScript Object parser
+Parses specified csv files into a Javascript object.
+
+## Mongodb Storage Utilities
+Contains functions which allow the storage of the Javascript objects which are
+parsed using the CSV to Javascript object parser. This also contains functions
+which ensures the contents of csv files wouldn’t be duplicated in the database.
+It also has functions for determining whether a calls processed file is
+Enhanced or not, and upserts it to the database if it is. Another feature of
+this module includes a mechanism which includes scores which are scored more
+than once in a separate database called `weirdScores` to ensure that all data
+will be counted as is (while also ensuring the uniqueness of these scores).
---
+++ test/test.js
@@ -0,0 +1,121 @@
+var assert = require('assert');
+var ccpo = require('../lib').createCallsProcessedObject;
+var scpm = require('../lib').storeCallsProcessedToMongo;
+var MongoClient = require('mongodb').MongoClient;
+
+var options = {
+  mongodb: true,
+  fieldFormat: [
+    { name: 'processingStart', type: 'Date', zone: '+0000' },
+    { name: 'processingEnd', type: 'Date', zone: '+0000' },
+    { name: 'callStartTime', type: 'Date', zone: '+0000' },
+    { name: 'elsProcessingStart', type: 'Date', zone: '+0000' },
+    { name: 'elsProcessingEnd', type: 'Date', zone: '+0000' },
+    { name: 'elsProcessingEnd', type: 'Date', zone: '+0000' },
+    { name: 'processingTimeSec', type: 'Number' },
+    { name: 'elsProcessingTimeSec', type: 'Number' },
+    { name: 'accountId', type: 'Number' },
+    { name: 'index', type: 'Number' },
+    { name: 'callDuration', type: 'Number' },
+    { name: '_id', type: 'Number' },
+  ]
+};
+
+describe('LSCP Parser', function () {
+  it('should define ccpo as a function', function () {
+    assert.equal(typeof ccpo, 'function');
+  });
+  it('should create an array with 30 elements', function (done) {
+    var csvPath = 'test/ECPwiththirtyelements.csv';
+    console.log('Processing file:', csvPath);
+    ccpo(csvPath, options, function (callsProcessedObjects) {
+      assert.equal(callsProcessedObjects.length, 30);
+      done();
+    });
+  });
+});
+
+describe('MongoDB data store Tests.', function () {
+  var ecpOne = 'test/ECPwiththirtyelements.csv';
+  var ecpTwo = 'test/ECPwithconflict.csv';
+  var scores;
+  var mongodb;
+  //var lcpOne = '';
+  //var lcpTwo = '';
+  beforeEach(function (done) {
+    var objectsCreated = 0;
+    var droppedDb = false;
+    var connectedToDb = false;
+    var isProcessingFinished = function () {
+      if (droppedDb && connectedToDb) {
+        done();
+      }
+    };
+    MongoClient.connect('mongodb://localhost/test', function (err, db) {
+      if (err) throw err;
+      connectedToDb = true;
+      mongodb = db;
+      scores = mongodb.collection('scores');
+      mongodb.dropDatabase(function (err, result) {
+        droppedDb = true;
+        isProcessingFinished();
+      });
+      isProcessingFinished();
+    });
+  });
+  it('should store the 30 elements contained in ' + ecpOne + '.', function (done) {
+    var afterInsert = function () {
+      scores.count(function (err, count) {
+        assert.equal(30, count);
+        return done();
+      });
+    };
+    scpm(ecpOne, mongodb, afterInsert);
+  });
+  it('should not store objects contained in ' + ecpOne + ' twice.', function (done) {
+    var insertCount = 0;
+    var afterInsert = function () {
+      ++insertCount;
+      if (insertCount === 2) {
+        scores.count(function (err, count) {
+          assert.equal(30, count);
+          return done();
+        });
+      }
+    };
+    scpm(ecpOne, mongodb, afterInsert);
+    scpm(ecpOne, mongodb, afterInsert);
+  });
+  it('should store a record in weirdScores.', function (done) {
+    var afterInsert = function () {
+      var selector = { _id: 'baed5aa21a92467021b0faf82837e9eaf93c4509' };
+      scores.findOne(selector,
+          function (err, obj) {
+        if (err) throw err;
+        assert.equal(obj._id, selector._id);
+        return done();
+      });
+    };
+    scpm(ecpTwo, mongodb, afterInsert);
+  });
+  it('should not store objects twice.', function (done) {
+    this.timeout(0);
+    var insertCount = 0;
+    var afterInsert = function () {
+      ++insertCount;
+      if (insertCount === 2) {
+        scores.count(function (err, count) {
+          if (err) throw err;
+          assert.equal(count, 3);
+          return done();
+        });
+      }
+      scores.count(function (err, count) {
+        if (err) throw err;
+        assert.equal(count, 3);
+        scpm(ecpTwo, mongodb, afterInsert);
+      });
+    };
+    scpm(ecpTwo, mongodb, afterInsert);
+  });
+});
