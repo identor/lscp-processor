771bec39f4835ae6c08ea4d5a7b83feea0079df6
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-02-28 13:05:43 +0800

    Added lodash as a dependency, counting the scores of a particular scorer now available (exposed through the queries object). Fixed not returning records as per scorer and date.

--- test/queries.js
+++ test/queries.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+var lArray = require('lodash/array');
+var lColl = require('lodash/collection');
 var queries = require('../lib/queries.js');
 var DbUtils = require('../lib/db-utils.js').DbUtils;
 var common = require('./common.js');
@@ -16,15 +18,53 @@ describe('Call Count Tests', function () {
     var finished = function (callsProcessed) {
       done();
     };
-    queries.callsProcessedCount(scores, finished);
+    queries.callsProcessed(scores, finished);
   });
   it('should aggregate the scores in CPone.csv.', function (done) {
     var finished = function (callsProcessed) {
-      assert.equal(callsProcessed.length, 26);
+      var selector = { scorer: 'ARPIRAbellanesAU' };
+      var selectorIndex = lArray.findIndex(callsProcessed, selector);
+      var selectorCallCount = callsProcessed[selectorIndex].callCount;
+      assert.equal(selectorCallCount, 2);
+      selector = { scorer: 'ARPIAJBuenaAU' };
+      selectorCallCount = lColl.filter(callsProcessed, selector).length;
+      assert.equal(selectorCallCount, 2);
+      assert.equal(callsProcessed.length, 27);
       done();
     };
     var saved = function () {
-      queries.callsProcessedCount(scores, finished);
+      queries.callsProcessed(scores, finished);
+    };
+    DbUtils.saveToDb(common.cpOne, scores, common.formatting, saved);
+  });
+  it('should aggregate the scores of \'ARPIAbellanesAU\' in CPone.csv.', function (done) {
+    var selector = { scorer: 'ARPIRAbellanesAU' };
+    var finished = function (callsProcessed) {
+      assert.equal(callsProcessed[0].callCount, 2);
+      assert.equal(callsProcessed.length, 1);
+      done();
+    };
+    var saved = function () {
+      queries.scorerCallsProcessed(scores, selector.scorer, finished);
+    };
+    DbUtils.saveToDb(common.cpOne, scores, common.formatting, saved);
+  });
+  it('should aggregate the scores of \'ARPIAJBuenaAU\' in CPone.csv.', function (done) {
+    var selector = { scorer: 'ARPIAJBuenaAU' };
+    var finished = function (callsProcessed) {
+      var dayOneCount = lColl.pluck(
+        lColl.filter(callsProcessed, { date: { day: 1 } } ), 'callCount'
+      )[0];
+      var dayTwoCount = lColl.pluck(
+        lColl.filter(callsProcessed, { date: { day: 2 } } ), 'callCount'
+      )[0];
+      assert.equal(dayOneCount, 2);
+      assert.equal(dayTwoCount, 1);
+      assert.equal(callsProcessed.length, 2);
+      done();
+    };
+    var saved = function () {
+      queries.scorerCallsProcessed(scores, selector.scorer, finished);
     };
     DbUtils.saveToDb(common.cpOne, scores, common.formatting, saved);
   });
